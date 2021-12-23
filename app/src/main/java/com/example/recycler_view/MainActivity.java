bb05844ec54800f278864413a4428e15c49a556e
Ali <>
2021-12-24 00:00:14 +0500

    third commit

--- app/src/main/java/com/example/recycler_view/MainActivity.java
+++ app/src/main/java/com/example/recycler_view/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
 import android.widget.Adapter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -32,5 +33,45 @@ public class MainActivity extends AppCompatActivity {
         adapter.notifyDataSetChanged();
     }
 
+
+    private void initData() {
+        userList = new ArrayList<>();
+
+        userList.add(new ModelClass(R.drawable.boyy,"Ali","How are you?","10:45 am","_______________________________________"));
+
+        userList.add(new ModelClass(R.drawable.boyy,"Aslam","I am fine","15:08 pm","_______________________________________"));
+
+        userList.add(new ModelClass(R.drawable.boyy,"Kashif","You Know?","1:02 am","_______________________________________"));
+
+        userList.add(new ModelClass(R.drawable.girl,"salma","How are you?","12:55 pm","_______________________________________"));
+
+        userList.add(new ModelClass(R.drawable.boyy,"Suleman","This is Easy","13:50 am","_______________________________________"));
+
+        userList.add(new ModelClass(R.drawable.boyy,"basit","I am Don","1:08 am","_______________________________________"));
+
+        userList.add(new ModelClass(R.drawable.boyy,"Qasim","You Know this?","4:02 am","_______________________________________"));
+
+        userList.add(new ModelClass(R.drawable.girl,"Binish","How ?","11:55 pm","_______________________________________"));
+
+        userList.add(new ModelClass(R.drawable.boyy,"Hamail","How are you?","10:45 am","_______________________________________"));
+
+        userList.add(new ModelClass(R.drawable.boyy,"sarim","I am fine","15:08 pm","_______________________________________"));
+
+        userList.add(new ModelClass(R.drawable.boyy,"Sam","You Know?","1:02 am","_______________________________________"));
+
+        userList.add(new ModelClass(R.drawable.boyy,"Dilawar","How are you?","12:55 pm","_______________________________________"));
+
+        userList.add(new ModelClass(R.drawable.boyy,"Sameer","This is Easy","13:50 am","_______________________________________"));
+
+        userList.add(new ModelClass(R.drawable.boyy,"Karan","I am Don","1:08 am","_______________________________________"));
+
+        userList.add(new ModelClass(R.drawable.boyy,"Asim","You Know this?","4:02 am","_______________________________________"));
+
+        userList.add(new ModelClass(R.drawable.boyy,"Nawaz","How ?","11:55 pm","_______________________________________"));
+
+
+
+
+    }
 
     }
--- app/src/main/java/com/example/recycler_view/ModelClass.java
+++ app/src/main/java/com/example/recycler_view/ModelClass.java
@@ -7,5 +7,36 @@ public class ModelClass {
     private String textview3;
     private String divider;
 
+    ModelClass (int imageview ,String textview1,String textview2,String textview3,String divider)
+    {
+        this.imageview=imageview;
+        this.textview1=textview1;
+        this.textview2=textview2;
+        this.textview3=textview3;
+        this.divider=divider;
+    }
+
+    public int getImageview() {
+        return imageview;
+    }
+
+    public String getTextview1() {
+        return textview1;
+    }
+
+    public String getDivider()
+    {
+        return divider;
+    }
+
+
+    public String getTextview2() {
+        return textview2;
+    }
+
+    public String getTextview3() {
+        return textview3;
+    }
+
 
 }
