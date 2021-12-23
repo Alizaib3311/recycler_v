5d5cd2483ff3fdf9254eb5c3227fd960a4f85009
Ali <>
2021-12-23 23:50:37 +0500

    second

--- app/src/main/java/com/example/recycler_view/MainActivity.java
+++ app/src/main/java/com/example/recycler_view/MainActivity.java
@@ -1,14 +1,36 @@
 package com.example.recycler_view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Adapter;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
+    RecyclerView mrecyclerView;
+    LinearLayoutManager layoutManager;
+    List<ModelClass> userList;
+    Adapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-}
+    private void initRecyclerView() {
+        mrecyclerView = findViewById(R.id.RecyclerView);
+        layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(RecyclerView.VERTICAL);
+        mrecyclerView.setLayoutManager(layoutManager);
+        adapter = new Adapter(userList);
+        mrecyclerView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+    }
+
+
+    }
---
+++ app/src/main/java/com/example/recycler_view/ModelClass.java
@@ -0,0 +1,11 @@
+package com.example.recycler_view;
+
+public class ModelClass {
+    private int imageview;
+    private String textview1;
+    private String textview2;
+    private String textview3;
+    private String divider;
+
+
+}
